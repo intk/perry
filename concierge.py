d8de949e0a5b8dbf1a0498a21a8299bcd186d372
Sergey Polzunov <sergey@polzunov.com>
2014-05-07 21:30:00 +0200

    tweaks

--- concierge.py
+++ concierge.py
@@ -13,25 +13,23 @@ class Concierge(DatagramProtocol):
     CMD_PING = "magic-ping"
     CMD_PONG = "magic-pong"
 
+    DISCOVERY_INVERVAL = 1 # sec
+
     def __init__(self, host, port, new_neighbour_callback=None):
         self.host = host
         self.port = port
         self.new_neighbour_callback = new_neighbour_callback
 
     def startProtocol(self):
-        # Set the TTL>1 so multicast will cross router hops:
-        #self.transport.setTTL(5)
-
         self.transport.socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, True)
 
-        #self.periodic_shout = LoopingCall(self.sendDatagram)
-        #self.periodic_shout.start(1)
+        self.periodic_shout = LoopingCall(self.sendDatagram)
+        self.periodic_shout.start(self.DISCOVERY_INVERVAL)
 
 
     def sendDatagram(self):
         log.msg("sending discovery datagram")
         self.transport.write(self.CMD_PING, ('255.255.255.255', self.port)) # <- write to broadcast address here
-        #self.transport.write(self.CMD_PING, self.MULTICAST_ADDR)
 
     def datagramReceived(self, datagram, address):
 
@@ -46,6 +44,8 @@ class Concierge(DatagramProtocol):
             self.transport.write(self.CMD_PONG, address)
         elif datagram.startswith(self.CMD_PONG):
             # someone reply to our publish message
+
+            #FIXME: dummy neighbour hash generation
             self.new_neighbour_callback(Neighbour(str(uuid.uuid4()), address[0]))
             log.msg("Got client: %s, %s" % (address[0], address[1]))
 
